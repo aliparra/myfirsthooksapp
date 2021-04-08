7dd7cce3621cfed7a2010d5de13ee36268569d3f
Alicia Parra <alicia@redparra.com>
2021-04-08 19:38:02 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My first app with react hooks
         </a>
       </header>
     </div>
